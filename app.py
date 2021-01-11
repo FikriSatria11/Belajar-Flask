8740c9d2f4a5dd733b9c5e4a3541187aa33d95aa
FikriSatria11 <muhfikrisatria@gmail.com>
2021-01-11 17:43:42 +0700

    add new template

--- app.py
+++ app.py
@@ -12,14 +12,21 @@ def main():
     # seleksi kondisi if-else
     perasaan = "nda tau" # jika senang maka dia juga cinta, jika tidak maka dia sedih
 
-    #set variable
-
-
+    # set variable\
+    # langsung di template html
     return render_template("index.html", nilai = nilaiku, day = hari, feeling = perasaan)
 
 @app.route("/ini_index")
 def myindex():
     return "<h1>ini index!</h1> </br> <h3>selamat datang di main</h3>"
+
+@app.route("/about")
+def aboutku():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contactku():
+    return render_template("contact.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
