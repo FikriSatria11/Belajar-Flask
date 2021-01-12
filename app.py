33231fb3b4a015d3fbf00797d311fd4f9afb5816
FikriSatria11 <muhfikrisatria@gmail.com>
2021-01-12 19:22:15 +0700

    url_for & redirect

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, template_render, request, session
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "inisecretkeyku2021"
@@ -6,6 +6,23 @@ app.config["SECRET_KEY"] = "inisecretkeyku2021"
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/redirect-about")
+def redirect_about():
+    return redirect(url_for('about'))
+
+@app.route("/redirect-contact")
+def redirect_contact():
+    return redirect(url_for('contact'))
+
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0")
