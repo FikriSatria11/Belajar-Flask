8692fddaa6bdc9f2a3f89a064d94c899c08265a5
FikriSatria11 <muhfikrisatria@gmail.com>
2021-01-12 21:04:10 +0700

    login dan logout

--- app.py
+++ app.py
@@ -1,19 +1,52 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request, session
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "inisecretkeyku2021"
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    if "email" in session:
+        return redirect(url_for('sukses_req'))
+    else:
+        # Jika tombol button diklik -> request Post
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            # jika email dan password benar
+            if email == 'admin@gmail.com' and password == 'pass':
+                session['email'] = email
+                session['password'] = password
+                return redirect(url_for('sukses_req'))
+            # jika email dan password salah
+            else:
+                # harus login lagi
+                return render_template("index.html")
+
+        return render_template("index.html")
+
+@app.route("/sukses")
+def sukses_req():
+    nilai = "Anda sukses login!"
+    return render_template("sukses.html", nilai = nilai)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    if "email" in session:
+        return render_template("about.html")
+    else:
+        return redirect(url_for('index'))
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    if "email" in session:
+        return render_template("contact.html")
+    else:
+        return redirect(url_for('index'))
+
+@app.route("/logout")
+def logout_akun():
+    session.pop("email", "password")
+    return redirect(url_for('index'))
 
 @app.route("/redirect-about")
 def redirect_about():
@@ -22,7 +55,6 @@ def redirect_about():
 @app.route("/redirect-contact")
 def redirect_contact():
     return redirect(url_for('contact'))
-
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0")
